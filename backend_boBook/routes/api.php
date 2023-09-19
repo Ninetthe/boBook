72730a1da9301fb9217900b8fd3d00950dc05ffe
NINETTHE <raissa.escobar1408@gmail.com>
2023-09-19 13:46:19 +0200

    Route api modification Login

--- backend_boBook/routes/api.php
+++ backend_boBook/routes/api.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/register', '\App\Http\Controllers\Auth\RegisterUserController@store');
+Route::post('/login', '\App\Http\Controllers\Auth\AuthenticatedSessionController@store');
+Route::post('/logout', '\App\Http\Controllers\Auth\AuthenticatedSessionController@destroy');
 
 Route::group(['prefix' => 'users'], function () {
     Route::get('/', 'App\Http\Controllers\UserController@index');
